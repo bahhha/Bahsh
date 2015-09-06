b5c5f68b462cf49a253fbbfb0557147d86078bbe
bahhha <bahhha.y@gmail.com>
2015-09-06 11:25:38 +0900

    エスケープの方法をちょこっと変更
    text()を実行するだけでいいっぽい。
    あと、連続したスペースを全部&nbsp;に変換したりしてたけど、そうすると右端で折り返してくれなくなっちゃったのでやめた。

--- common/js/console.js
+++ common/js/console.js
@@ -4,17 +4,15 @@ $(function() {
     $("#input-dummy").focus();
   });
   $("#input-dummy").on("input", function(e) {
-    var val = $("#input-dummy").val();
-    val = $(".command:last-of-type").html(val).text();
-    $(".command:last-of-type").text(val);
+    $(".command:last-of-type").text($("#input-dummy").val().replace(/ +/g, " "));
   });
   $("#input-dummy").keypress(function(e) {
     if (e.which == 13) {
       // enter key
-      $("#input-dummy").val("");
       $("#console").append("<p class='command'></p>");
-      $("#scroll-area").scrollTop($("#console").height);
+      $("#input-dummy").val("");
     }
+    $("#scroll-area").scrollTop($("#scroll-area").get(0).scrollHeight);
   });
   $("#input-dummy").focus();
 });
