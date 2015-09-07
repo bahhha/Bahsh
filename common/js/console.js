8060c067f9c2838d508fdf69741b6e847260b8a1
bahhha <bahhha.y@gmail.com>
2015-09-07 23:25:32 +0900

    IE死すべし
    カーソル＋変換候補が正しい位置に出るようにした。
    カーソル位置についてはhttp://qiita.com/yuku_t/items/fb92e173120d7b2e49ed
    変換候補については、入力中っぽいカーソルが出る位置の真後ろにtextareaがあるだけ。
    IEではカーソルが表示されちゃう。
    IE死すべし。

--- common/js/console.js
+++ common/js/console.js
@@ -1,26 +1,57 @@
+function resetConsoleCaretPosition() {
+  var $input = $("#input-line");
+  var $checker = $("#caret-checker");
+  var start = $input.get(0).selectionStart;
+  var val = $input.val();
+  var span = document.createElement('span');
+  span.innerHTML = "_";
+  $checker.text(val.substring(0, start));
+  $checker.append(span);
+  var pos = $(span).position();
+  var style = [
+    ".command:last-of-type::after{",
+    "top:" + pos.top + "px;",
+    "left:" + pos.left + "px;",
+    "}"
+  ];
+  $("#caret-checker-style").text(style.join(""));
+}
 $(function() {
   $("#scroll-area").click(function(e) {
     e.preventDefault();
-    $("#input-dummy").focus();
+    $("#input-line").focus();
   });
-  $("#input-dummy").on("input", function(e) {
-    $(".command:last-of-type").text($("#input-dummy").val().replace(/^ +/, "").replace(/ +/g, " "));
+  $("#input-line").on("input", function(e) {
+    var val = $(e.target).val();
+    $("p:last-of-type").text(val);
+    $(e.target).height($("p:last-of-type").get(0).offsetHeight);
   });
-  $("#input-dummy").keypress(function(e) {
+  $("#input-line").keyup(function(e) {
+    $("#scroll-area").scrollTop($("#scroll-area").get(0).scrollHeight);
+    resetConsoleCaretPosition();
+  });
+  $("#input-line").keydown(function(e) {
     if (e.which == 13) {
       // enter key
-      var words = $(".command:last-of-type").text().split(" ");
+      var val = $("#input-line").val();
+      $("p:last-of-type").text(val);
+      var words = val.split(" ");
       var command = words.shift();
-      $("#console").append("<p></p>");
-      if (command == "echo") {
-        $("p:last-child").text(words.join(" "));
+      console.log("'" + command + "'");
+      if (!command) {} else if (command == "echo") {
+        $("#console").append("<p></p>");
+        $("p:last-of-type").text(words.join(" "));
       } else {
-        $("p:last-child").text("command " + command + " not found");
+        $("#console").append("<p></p>");
+        $("p:last-of-type").text("command '" + command + "' not found");
       }
+      $("#input-line").val("");
       $("#console").append("<p class='command'></p>");
-      $("#input-dummy").val("");
+      $("#scroll-area").scrollTop($("#scroll-area").get(0).scrollHeight);
+      resetConsoleCaretPosition();
+      return false;
     }
-    $("#scroll-area").scrollTop($("#scroll-area").get(0).scrollHeight);
   });
-  $("#input-dummy").focus();
+  $("#input-line").focus();
+  resetConsoleCaretPosition();
 });
